9d033d34249f649710253219d1f32bffc2d752de
intwari kagame fred <intwari.k.fred@gmail.com>
2024-03-21 18:20:05 -0700

    Sharing State Between Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
 
-function App() {
+function Panel({title, children}) {
+  const [isActive, setIsActive] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="panel">
+      <h3>{title}</h3>
+      {isActive ?(
+        <p>{children}</p>
+      ) : (
+        <button onClick={() => setIsActive(true)}>Show</button>
+      )}
+    </section>
   );
 }
 
-export default App;
+export default function Accordion() {
+  return (
+    <>
+    <h2>Almaty, Kazakhstan</h2>
+    <Panel title="About">
+      with the popupulation of 2 million, almatyy is khazakistan's largest city. from 1929 to 1997, it was its capital city.
+    </Panel>
+    <Panel title="Ethymology">
+      The name comes from <span lang="kk-kz">алма</span>, the kazakh word for "apple" and is often translated as "full of apples ". In fact, the region surroundin almaty is thought to be the ancental home of the apple, and the wild <i lang="la">Malus sieversii</i> is considered a likely candidate for the ancestor of the modern domestic apple.
+    </Panel>
+    </>
+  );
+}
